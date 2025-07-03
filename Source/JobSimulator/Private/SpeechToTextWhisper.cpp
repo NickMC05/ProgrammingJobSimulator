e7a4a6b5a7f86939908b4643fdb1537bad5a574f
Nicknation Games <166688051+NickNationGames@users.noreply.github.com>
2025-07-03 15:46:57 +0800

    Minor Changes

---
+++ Source/JobSimulator/Private/SpeechToTextWhisper.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SpeechToTextWhisper.h"
+
+// Sets default values
+ASpeechToTextWhisper::ASpeechToTextWhisper()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASpeechToTextWhisper::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASpeechToTextWhisper::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
